d6aed8931c0fd025b0b458acfe0ef959937ea9ed
fpsgonet <playcsolllll@proton.me>
2025-02-12 22:11:17 +0800

    update

--- 隐身-1.0.user.js
+++ 隐身-1.0.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         隐身（和修改昵称不能同时开启）
+// @name         隐身
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  try to take over the world!
+// @version      0.1
+// @description  隐身
 // @author       You
 // @match        https://fpsgo.net/*
 // @grant        none
@@ -11,11 +11,38 @@
 (function() {
     'use strict';
 
-    // Your code here...
     const originalSend = WebSocket.prototype.send;
 
     // 你想修改的 rate 值
     const newRate = "1000000"; // 新的 rate 值
+
+    // 十六进制编码转换函数
+    function stringToHex(str) {
+        let hex = '';
+        for (let i = 0; i < str.length; i++) {
+            hex += '\\x' + str.charCodeAt(i).toString(16).padStart(2, '0');
+        }
+        return hex;
+    }
+
+    // 十六进制转二进制
+    function hexToBuffer(hex) {
+        let bytes = [];
+        for (let i = 0; i < hex.length; i += 2) {
+            bytes.push(parseInt(hex.substr(i, 2), 16));
+        }
+        return new Uint8Array(bytes).buffer;
+    }
+
+    // 十六进制转字符串
+    function bufferToHex(buffer) {
+        let hex = '';
+        let view = new DataView(buffer);
+        for (let i = 0; i < view.byteLength; i++) {
+            hex += view.getUint8(i).toString(16).padStart(2, '0');
+        }
+        return hex;
+    }
 
     WebSocket.prototype.send = function(data) {
         try {
@@ -26,14 +53,14 @@
                 if (textData.includes("5c72617465")) { // 5c72617465 是 "rate" 的 HEX
                     console.log("拦截到 WebSocket 发送的数据:", textData);
 
-                    // 使用正则表达式匹配 rate 后的数值
-                    let match = textData.match(/5c72617465[0-9a-f]*/);
+                    // 使用正则表达式匹配 rate 后的数值 (只替换数字部分)
+                    let match = textData.match(/5c72617465([0-9a-f]+)/);
                     if (match) {
                         // 动态转换 newRate 为 HEX
                         let newRateHex = stringToHex(newRate);
 
-                        // 替换 rate 后的数据
-                        textData = textData.replace(/(5c72617465)[0-9a-f]*/, `$1${newRateHex}`);
+                        // 替换 rate 后的数字部分
+                        textData = textData.replace(/(5c72617465)[0-9a-f]+/, `$1${newRateHex.slice(2)}`);
 
                         // 转回二进制并发送
                         data = hexToBuffer(textData);
@@ -47,32 +74,5 @@
 
         return originalSend.call(this, data);
     };
-
-    // 以下是辅助函数：将字符串转换为 HEX 和 HEX 转回 Buffer
-    function stringToHex(str) {
-        let hex = '';
-        for (let i = 0; i < str.length; i++) {
-            hex += '\\x' + str.charCodeAt(i).toString(16);
-        }
-        return hex.replace(/\\x/g, '');
-    }
-
-    function hexToBuffer(hex) {
-        let array = [];
-        for (let i = 0; i < hex.length; i += 2) {
-            array.push(parseInt(hex.substr(i, 2), 16));
-        }
-        return new Uint8Array(array).buffer;
-    }
-
-    function bufferToHex(buffer) {
-        const byteArray = new Uint8Array(buffer);
-        let hex = '';
-        byteArray.forEach(byte => {
-            hex += byte.toString(16).padStart(2, '0');
-        });
-        return hex;
-    }
-
 
 })();
